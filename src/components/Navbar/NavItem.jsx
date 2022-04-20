2ccf486b9ca46ea158ccf6957f8e218de5a51bad
Adithyan A <deepaadithyan56@gmail.com>
2022-04-20 22:17:39 +0530

    NAV-ITEM Component for desktop and mobile added

---
+++ src/components/Navbar/NavItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function NavItem(props) {
+  return (
+    <>
+      <NavLink
+        className="hover:scale-105 ease-in-out duration-300 text-gray-600 hover:text-gray-700 dark:text-gray-200 dark:hover:text-white "
+        to={props.url}
+      >
+        {props.title}
+      </NavLink>
+    </>
+  );
+}
+
+NavItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
