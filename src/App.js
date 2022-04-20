fda0d7a5f3a68c073c095bdd6818ba76bd492943
Adithyan A <deepaadithyan56@gmail.com>
2022-04-20 22:18:27 +0530

    ROUTES AND SKELETON ADDED

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import News from "./components/News";
 
-export default class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
+export default function App() {
+  return (
+    // TODO: Change all navlinks to a dropdown called categories
+    // TODO: Add language dropdown lo navbar
+    // TODO: Add country dropdown to navbar
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<News title="Trending News" />} />
+
+          <Route path="/business" element={<News title="Business" category="business" />} />
+          <Route
+            path="/entertainment"
+            element={<News title="Entertainment" category="entertainment" />}
+          />
+          <Route path="/general" element={<News title="General" category="general" />} />
+          <Route path="/health" element={<News title="Health" category="health" />} />
+          <Route path="/science" element={<News title="Science" category="science" />} />
+          <Route path="/sports" element={<News title="Sports" category="sports" />} />
+          <Route path="/technology" element={<News title="Technology" category="technology" />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
+export default function Layout() {
+  return (
+    <>
+      <div className="relative bg-slate-200 dark:bg-slate-900 min-h-screen">
+        <Navbar />
+        <main className="pt-20 mx-auto max-w-7xl w-11/12">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
