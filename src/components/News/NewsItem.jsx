d479138fa6769ae11829e988b439f2098a498ded
Adithyan A <deepaadithyan56@gmail.com>
2022-04-26 21:41:27 +0530

    Converted to functional component and propTypes added

--- src/components/News/NewsItem.jsx
+++ src/components/News/NewsItem.jsx
@@ -1,28 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export default class NewsItem extends Component {
-  render() {
-    return (
-      <>
-        <div className="flex flex-col bg-white dark:bg-slate-600 w-72 rounded-xl">
-          <img className="object-cover h-44 rounded-t-xl" src={this.props.imageUrl} alt="Cover" />
-          <div className="flex flex-1 flex-col justify-between gap-3 items-start p-5">
-            <h1 className="font-medium text-slate-900 dark:text-white text-xl">
-              {this.props.title}
-            </h1>
-            <p className="text-slate-800 dark:text-slate-200">{this.props.description}</p>
-
-            <a
-              className="text-slate-900 mt-2 bg-slate-200 dark:text-white dark:bg-gray-400 w-36 py-2 text-center rounded-md hover:scale-105 duration-200"
-              href={this.props.newsUrl}
-              rel="noreferrer"
-              target="_blank"
-            >
-              Read More
+export default function NewsItem(props) {
+  return (
+    <>
+      <div className="flex flex-col overflow-hidden bg-white dark:bg-slate-600 w-72 rounded-xl">
+        <img
+          className="object-cover rounded-t-xl p-h-full font-bold text-xl text-center h-44 bg-slate-400 text-slate-200 dark:bg-slate-500 dark:text-slate-300"
+          src={props.imageUrl}
+          alt="News Promo "
+        />
+        <div className="flex flex-1 flex-col justify-between gap-3 items-start p-5">
+          <h1 className="font-medium text-slate-900 dark:text-white text-xl">
+            <a href={props.newsUrl} rel="noreferrer" target="_blank">
+              {props.title}
             </a>
-          </div>
+          </h1>
+          <p className="text-slate-800 dark:text-slate-200">{props.description}</p>
+          <a
+            className="text-slate-900 mt-2 bg-slate-200 dark:text-white dark:bg-gray-400 w-36 py-2 text-center rounded-md hover:scale-105 duration-200"
+            href={props.newsUrl}
+            rel="noreferrer"
+            target="_blank"
+          >
+            Read More
+          </a>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
+
+NewsItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  newsUrl: PropTypes.string.isRequired,
+};
