2a2e48e7806b28edb098f2bfc555c2387701f586
Adithyan A <deepaadithyan56@gmail.com>
2022-04-27 09:01:06 +0530

    Converted to functional component and isFullScreen props added

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
-import Spin from "./Spin";
+import React from "react";
+import PropTypes from "prop-types";
+import { ReactComponent as Spin } from "./spin.svg";
 
-export default class Spinner extends Component {
-  componentDidMount() {
-    document.documentElement.style.overflowY = "hidden";
-  }
-
-  componentWillUnmount() {
-    document.documentElement.style.overflowY = "scroll";
-  }
-
-  render() {
-    return (
-      <>
-        <div className="bg-slate-200 opacity-70 backdrop-blur-md dark:bg-slate-900 fixed top-0 left-0 right-0  h-screen flex justify-center items-center">
-          <Spin />
-        </div>
-      </>
-    );
-  }
+export default function Spinner(props) {
+  return (
+    <>
+      <div
+        className={`flex justify-center items-center ${
+          props.isFull
+            ? "bg-slate-200 opacity-70 backdrop-blur-md dark:bg-slate-900 fixed top-0 left-0 right-0 h-screen"
+            : "p-3"
+        }`}
+      >
+        <Spin
+          className={`rounded-full animate-spin mr-2 text-gray-400 dark:text-gray-600 fill-blue-600 ${
+            props.isFull ? "w-12 h-12" : "w-10 h-10"
+          }`}
+        />
+      </div>
+    </>
+  );
 }
+
+Spinner.defaultProps = {
+  isFull: false,
+};
+
+Spinner.propTypes = {
+  isFull: PropTypes.bool,
+};
